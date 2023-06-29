ea7347696f6d233a30468d9a56115c88a5ddb107
jahrulesfrancis <longsaarmuknaan@gmail.com>
2023-06-30 00:37:51 +0100

    Update project section

--- src/Components/Homepage/landingPage.jsx
+++ src/Components/Homepage/landingPage.jsx
@@ -1,7 +1,15 @@
-import { Box, Heading, Stack, VStack, Text, useMediaQuery, Image, Button, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Heading, Stack, VStack, Text, useMediaQuery, Image, Button, Grid, Spacer } from "@chakra-ui/react";
 import NavBar from "../Common/NavBar";
 import ProfilePicture from "../Assets/Images/Portcover.jpg"
 import styled from "@emotion/styled";
+import ProjectCard from "../Common/ProjectCard";
+import Project1 from "../Assets/Images/Img.svg";
+import project2 from "../Assets/Images/Img(1).svg";
+import Project3 from "../Assets/Images/Img(2).svg";
+import project4 from "../Assets/Images/Img(3).svg";
+import Testimmonials from "../Common/Testimonials/Testimonials";
+
+
 
 function LandingPage() {
     const [isMobileDevice] = useMediaQuery('(max-width: 700px)')
@@ -28,10 +36,20 @@ function LandingPage() {
                         <Image borderRadius="10px" width="350px" src={ProfilePicture} alt="Longsaar Francis" />
                     </Box>
 
-                    <div className="project-highlight">
-                        <Heading textAlign="center">My projects highlight</Heading>
+                    <div className="project-highlight-container" id="projects">
+                        <Heading mb="50px" textAlign="center" fontSize="30px">My projects highlight</Heading>
+                        <div className="project-list">
+                            <ProjectCard title="wed design" client="Dele balogun" work="Book library" projectImage={Project1} />
+                            <ProjectCard title="Web development" client="Crac World" work="Real Esate website" projectImage={project2} />
+                            <ProjectCard title="wed design" client="Dele balogun" work="Book library" projectImage={Project1} />
+                            <ProjectCard title="Web development" client="Crac World" work="Real Esate website" projectImage={project2} />
+                            <ProjectCard title="wed design" client="Dele balogun" work="Book library" projectImage={Project1} />
+                            <ProjectCard title="Web development" client="Crac World" work="Real Esate website" projectImage={project2} />
+                        </div>
                     </div>
-
+                    <div className="testimonial">
+                        <Testimmonials />
+                    </div>
                 </Box>
             </Box>
         </Wrapper>
@@ -41,7 +59,27 @@ function LandingPage() {
 export default LandingPage;
 
 const Wrapper = styled.section`
-    .project-highlight{
+    .project-highlight-container{
         margin-top: 100px;
+    }
+    .project-highlight-container .project-list{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        justify-content: center;
+        margin: auto;
+        gap: 1em;
+        width: 750px;
+    }
+    @media(max-width: 700px){
+        .project-highlight-container .project-list{
+        width: auto;
+        grid-template-columns: 1fr;
+        }
+    }
+    @media(min-width: 700px)and (max-width: 1000px){
+        .project-highlight-container .project-list{
+        width: auto;
+        grid-template-columns: 1fr 1fr;
+        }
     }
 `
