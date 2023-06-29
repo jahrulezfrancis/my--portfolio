6195b508e7b4b00367960f40a39a18b630f93f72
jahrulesfrancis <longsaarmuknaan@gmail.com>
2023-06-29 05:14:53 +0100

    Add browser router component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { ChakraBaseProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraBaseProvider>
-    <App />,
-  </ChakraBaseProvider>
+  <BrowserRouter>
+    <ChakraBaseProvider>
+      <App />,
+    </ChakraBaseProvider>
+  </BrowserRouter>
 );
