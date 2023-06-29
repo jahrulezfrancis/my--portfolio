be2bb52eeb1644677a9da564ef267626566a3ff8
jahrulesfrancis <longsaarmuknaan@gmail.com>
2023-06-30 00:37:03 +0100

    Update project card

--- src/Components/Common/ProjectCard.jsx
+++ src/Components/Common/ProjectCard.jsx
@@ -1,14 +1,16 @@
-import { Image, Text, Heading } from "@chakra-ui/react";
+import { Card, Image, Text, Heading, Stack, Divider, ButtonGroup, Button, CardBody, CardFooter } from '@chakra-ui/react'
 
 const ProjectCard = (props) => {
     const { projectImage, title, client, work } = props;
     return (
-        <div className="project-card-container">
-            <Image width="500px" height="" src={projectImage} alt="project image" />
-            <Heading>{title}</Heading>
-            <Text>Client:{client}</Text>
-            <Text>Work: {work}</Text>
-        </div>
+        <>
+            <div className="project-card-container">
+                <Image width="320px" height="" src={projectImage} alt="project image" />
+                <Heading fontSize="2xl">{title}</Heading>
+                <Text color="#A8A8A8">Client:{client}</Text>
+                <Text color="#A8A8A8">Work: {work}</Text>
+            </div>
+        </>
     )
 }
 
