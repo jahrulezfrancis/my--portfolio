7cb7a973a944d325ec96539b291a3ab76bb8a057
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-07-08 23:58:31 +0100

    Changed container element

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import { Box, Button, Heading, Text } from '@chakra-ui/react';
 import { Route, Routes } from 'react-router-dom';
 import { LandingPage } from './Components/Homepage/Index';
-import NavBar from './Components/Common/NavBar';
 
 function App() {
   return (
-    <Box className='app'>
+    <div className='app'>
       <Routes>
         <Route path='/' element={<LandingPage />} />
       </Routes>
-    </Box>
+    </div>
   );
 }
 
