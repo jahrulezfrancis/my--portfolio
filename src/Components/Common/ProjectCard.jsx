83f5e9e6fb2a3fd017dd4092c18a9357b3a6e44f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-07-22 10:02:34 +0100

    change font size for nav text

--- src/Components/Common/ProjectCard.jsx
+++ src/Components/Common/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = (props) => {
         <>
             <div className="project-card-container">
                 <Image width="320px" height="" src={projectImage} alt="project image" />
-                <Heading fontSize="2xl">{title}</Heading>
+                <Heading fontSize="1.3rem">{title}</Heading>
                 <Text color="#A8A8A8">Client:{client}</Text>
                 <Text color="#A8A8A8">Work: {work}</Text>
             </div>
