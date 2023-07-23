d26207df666d970b22f78b2a9efa595c7e1c8ba1
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-07-23 01:02:36 +0100

    Change primary Chakraprovider

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ChakraBaseProvider, ColorModeScript } from '@chakra-ui/react';
+import { ChakraBaseProvider, ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
-import Theme from './Components/Common/Theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <ChakraBaseProvider>
-      <React.StrictMode>
-        <ColorModeScript initialColorMode={Theme.config.initialColorMode} />
+    <React.StrictMode>
+      <ChakraProvider>
         <App />
-      </React.StrictMode>
-    </ChakraBaseProvider>
+      </ChakraProvider>
+    </React.StrictMode>
   </BrowserRouter>
 );
