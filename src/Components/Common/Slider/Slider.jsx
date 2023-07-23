1267ffa8e1bb1d7b9e76569fe7437aea9addbb22
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-07-23 01:05:11 +0100

    Update slider styling, added dummy client message and name

--- src/Components/Common/Slider/Slider.jsx
+++ src/Components/Common/Slider/Slider.jsx
@@ -1,24 +1,34 @@
 import { useState } from "react";
-import { Button, Image, Stack } from "@chakra-ui/react";
+import { Button, Flex, Image, Stack, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 import Slide1 from "../../../Assets/Images/Img(1).svg"
 import Slide2 from "../../../Assets/Images/Img(2).svg"
 import Slide3 from "../../../Assets/Images/Img(3).svg"
+import styled from "@emotion/styled";
 
-function Slider() {
+
+function TestimonialSlider() {
+    const [isMobileDevice] = useMediaQuery('(max-width: 700px)');
+
     const [slide, setSlide] = useState(1)
 
     const Slides = [
         {
             index: 1,
-            Source: Slide1,
+            client: "Client 1",
+            message: "I really enjoy your services and will definitely recommend you for any type of web dev related jobs",
+            imgURL: Slide1,
         },
         {
             index: 2,
-            Source: Slide2,
+            client: "Client 2",
+            message: "This is where the client message will go. Cheers to better and honest testimonials",
+            imgURL: Slide2,
         },
         {
             index: 3,
-            Source: Slide3,
+            client: "Client 3",
+            message: "Somewhat different, This is where the client message will go. Cheers to better and honest testimonials",
+            imgURL: Slide3,
         }
     ]
 
@@ -34,25 +44,48 @@ function Slider() {
     const prevSlide = () => {
         if (slide > 1) {
             setSlide(slide - 1)
-        } else if (slide < 1) {
+        } else if (slide < 3) {
             setSlide(1);
         }
     }
 
     return (
-        <div slide-container>
-            {
-                Slides.map((carousel) => {
-                    return (
-                        (carousel.index === slide) && <Image src={carousel.Source} />
-                    )
-                })
-            }
-            <Stack direction="row" spacing={10}>
-                <Button onClick={prevSlide}>prev</Button>
-                <Button onClick={nextSlide}>next</Button>
-            </Stack>
-        </div>
+        <Wrapper>
+            <div slide-container>
+                <Heading fontSize="2rem">What my clients are saying about me!</Heading>
+                {
+                    Slides.map((carousel) => {
+                        return (
+                            (carousel.index === slide) &&
+                            <>
+                                <Flex gap={2}>
+                                    <Image width={"auto"} height={200} src={carousel.imgURL} />
+                                    <Stack spacing={10} width={isMobileDevice ? "320px" : "400px"}>
+                                        <Text>{carousel.client}</Text>
+                                        <Text>{carousel.message}</Text>
+                                    </Stack>
+                                </Flex>
+
+                            </>
+                        )
+                    })
+                }
+                <Stack direction="row" spacing={10}>
+                    <Button onClick={prevSlide}>prev</Button>
+                    <Button onClick={nextSlide}>next</Button>
+                </Stack>
+            </div>
+        </Wrapper>
     )
 }
-export default Slider;
+export default TestimonialSlider;
+
+
+const Wrapper = styled.section`
+        .slide-container{
+            display: flex;
+            flex-direction: column;
+            justify-contents: center;
+            align-contents: center;
+        }
+`
