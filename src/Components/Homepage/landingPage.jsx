d3288337df87fa24ddcb5eda6c16bd08b2257e32
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-07-29 19:09:14 +0100

    Update bio and expertise

--- src/Components/Homepage/landingPage.jsx
+++ src/Components/Homepage/landingPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Stack, Text, Image, Spacer, Divider, IconButton, Button, List, ListItem, useMediaQuery } from "@chakra-ui/react";
+import { Box, Heading, Stack, Text, Image, Spacer, Icon, List, ListItem, useMediaQuery } from "@chakra-ui/react";
 import NavBar from "../Common/NavBar";
 import ProfilePicture from "../../Assets/Images/Portcover.jpg"
 import styled from "@emotion/styled";
@@ -25,13 +25,14 @@ function LandingPage() {
                     <Spacer boxSize="100px" />
                     <Box id="about" display="flex" flexWrap="wrap" justifyContent={"space-around"}>
                         <Stack spacing={2} align="start" justify="center">
-                            <Heading fontSize="25px">have you heard about me? </Heading>
+                            <Heading fontSize="1.75rem">Have you heard about me? </Heading>
                             <Text className="about" width={isMobileDevice ? "320px" : isTabletDevice ? "500px" : "600px"} color="#A8A8A8">
-                                I am a front-end developer with a special focus on building JavaScript/ReactJS web systems with over 3
-                                years of experience building scalable, fast, responsive, and lightweight websites. I don't code for my personal
-                                consumption so I spend most of my time reading user-experience reports from surveys and use the results to make my
-                                development process as effective as possible for best user experience. I am solemn with deadlines and don't take projects
-                                of any type or size for granted. I believe in people's ideas as well as the need for keeping it private.
+                                I am a front-end engineer with a special focus on building React JS and WordPress web systems, with over 3 years
+                                of experience building scalable, fast, and visually stunning web solutions that elevate user experiences.
+                                I have a strong foundation in front-end engineering and a gracious passion for web development, which has enabled
+                                me to build dynamic and responsive websites that are precisely suited to the specific needs of each client.
+                                I am solemn with deadlines and don't take projects of any type or size for granted because I believe people's ideas are as important.
+                                I am also interested in Cyber Security and open to any growth opportunity.
                             </Text>
                         </Stack>
                         <Image borderRadius="10px" width="350px" src={ProfilePicture} alt="Longsaar Francis" />
@@ -39,19 +40,20 @@ function LandingPage() {
 
                     <div className="expertise-container">
                         <Stack className="expertise">
-                            <Heading fontSize="2rem">Things I Can Do For you</Heading>
+                            <Heading fontSize="1.75rem">Things I Can Do For you</Heading>
                             <List>
                                 <ListItem>I can build you a professional website for your brand or bussiness</ListItem>
-                                <ListItem>I can add third party tools to an existing website such as payment gateways, woocommerce, auto mail responders, etc.</ListItem>
-                                <ListItem>I can Provide maintenance, support, and secure your website using latest technologies to protect your data</ListItem>
+                                <ListItem>I can optimize existing websites performance </ListItem>
                                 <ListItem>I can create a personal / commercial fullstack responsive website using wordpress</ListItem>
                                 <ListItem>I can update your existing website and give it a mobile app speed with instant rendering</ListItem>
+                                <ListItem>I can integrate third party tools to an existing website such as payment gateways, woocommerce, auto mail responders, etc.</ListItem>
                             </List>
                         </Stack>
                     </div>
 
                     <div className="project-highlight-container" id="projects">
                         <Heading mb="50px" textAlign="center" fontSize="30px">My projects highlight</Heading>
+
                         <div className="project-list">
                             <ProjectCard title="wed design" client="Dele balogun" work="Book library" projectImage={Project1} />
                             <ProjectCard title="Web development" client="Crac World" work="Real Esate website" projectImage={project2} />
@@ -69,15 +71,21 @@ function LandingPage() {
                     </div>
                     <div>
                         <footer className="footer-container">
-                            <Heading color="#FF9142">Get In Touch With Me</Heading>
+                            <hr />
+                            <Heading fontSize="28px" color="#FF9142">Get In Touch With Me</Heading>
                             <address>Mai Adiko, Rayfield Jos</address>
                             <address>Email: owner@longsaarfrancis.com</address>
-                            <Stack direction="row">
-                                <IconButton as={FaLinkedin}></IconButton>
-                                <IconButton as={FaTwitter}></IconButton>
-                                <IconButton as={FaFacebook}></IconButton>
+                            <Stack fontSize="25px" direction="row">
+                                <a href="https://www.linkedin.com/in/francis-longsaar-1443b91b3" target="_">
+                                    <Icon as={FaLinkedin} />
+                                </a>
+                                <a href="https://twitter.com/Jahrulezfrancis" target="_">
+                                    <Icon as={FaTwitter} />
+                                </a>
+                                <a href="https://www.facebook.com/jahrulezfrancis" target="_">
+                                    <Icon as={FaFacebook} />
+                                </a>
                             </Stack>
-                            <hr />
                             <Text>Copyright 2023, deigned by Jahrulez</Text>
                         </footer>
                     </div>
@@ -136,10 +144,11 @@ const Wrapper = styled.section`
     }
     .divider-container > hr{
         display: flex;
-        width: 90%;
+        width: 60%;
         margin: 20px auto;
     }
     .footer-container{
+        margin-top: 100px;
         padding: 15px;
         // background: #020220;
         display: flex;
@@ -150,7 +159,10 @@ const Wrapper = styled.section`
     .footer-container > hr{
         display: flex;
         clear: both;
-        width: 90%;
+        width: 100%;
+    }
+    .footer-container > p, .footer-container > address{
+        font-size: 13px;
     }
     .testimonial-container{
         display: flex;
