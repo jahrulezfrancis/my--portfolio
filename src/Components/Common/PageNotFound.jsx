ce7a0dd3e157cf88c6f5a0604aeb504c47d9ea42
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-01 15:40:49 +0100

    Styled the 404 page component

--- src/Components/Common/PageNotFound.jsx
+++ src/Components/Common/PageNotFound.jsx
@@ -1,6 +1,8 @@
+import "./index.css"
+
 export default function PageNotFound() {
     return (
-        <div>
+        <div className="page-not-found-container">
             Francis is not sure of the page you are looking for, it seems to not to be available
         </div>
     )
