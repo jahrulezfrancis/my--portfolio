583d5315d7ac61c7ef8d28ca7b08ebc240ea2cd0
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-01 15:39:26 +0100

    Added navbar component and 404page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { LandingPage } from './Components/Homepage/Index';
+import PageNotFound from './Components/Common/PageNotFound';
+import NavBar from './Components/Common/NavBar';
 
 function App() {
   return (
     <div className='app'>
+      <NavBar />
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </div>
   );
