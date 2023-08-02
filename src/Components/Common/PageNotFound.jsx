9b4ff6acdbdb9e887f74f14718748a1e3ec8bc27
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-08-02 13:51:58 +0100

    Implementing redirection when page is not found

--- src/Components/Common/PageNotFound.jsx
+++ src/Components/Common/PageNotFound.jsx
@@ -1,9 +1,20 @@
 import "./index.css"
+import { useNavigate } from "react-router-dom"
+import { Text } from "@chakra-ui/react";
 
 export default function PageNotFound() {
+    const Navigate = useNavigate()
+
+    setTimeout(() => {
+        Navigate("/")
+    }, 5000);
+
     return (
         <div className="page-not-found-container">
-            Francis is not sure of the page you are looking for, it seems to not to be available
+            <Text style={{ textAlign: "center" }}>
+                Francis is not sure of the page you are looking for, it seems to not to be available. <br />
+                Redirecting...
+            </Text>
         </div>
     )
 }
