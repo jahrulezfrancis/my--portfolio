11d1d2f64a5fb5ccaf1ab6bb92b544725a963b91
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-09-07 15:38:46 +0100

    Update solid button properties

--- src/Components/Common/SolidButton.jsx
+++ src/Components/Common/SolidButton.jsx
@@ -1,11 +1,11 @@
 import { Button } from "@chakra-ui/react";
 
 
-function SolidButton({title, onClick}){
-return(
-    <div className="button-container">
-        <Button fontSize="17px" onClick={onClick} borderRadius={15} w="200px" p="15px 30px" bgColor={"#FF9142"}>{title}</Button>
-    </div>
-)
+function SolidButton({ title, onClick, color }) {
+    return (
+        <div className="button-container">
+            <Button color={color} fontSize="17px" onClick={onClick} borderRadius={15} w="200px" p="15px 30px" bgColor={"#DD226D"}>{title}</Button>
+        </div>
+    )
 }
 export default SolidButton;
