1c9ba9c6f16cbf6daab2dee8a9fa201647902f2b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-09-11 22:17:02 +0100

    Update button and project card layout

--- src/Components/Common/ProjectCard.jsx
+++ src/Components/Common/ProjectCard.jsx
@@ -1,25 +1,26 @@
-import { Image, Text, Heading, } from '@chakra-ui/react'
+import { Image, Text, Heading, Link, } from '@chakra-ui/react'
+import SolidButton from './SolidButton';
 import { motion } from 'framer-motion';
 import "./index.css"
 
-const ProjectCard = (props) => {
-    const { projectImage, title, client, work, className, id } = props;
+const ProjectCard = ({ projectImage, title, client, projectType, className, id }) => {
     return (
         <>
             <motion.div
                 className=""
-                layoutId={id}
+                // layoutId={id}
                 // layout
-                initial={{ opacity: 0, x: -100 }}
-                animate={{ opacity: 2, x: 0 }}
-                exit={{ opacity: 0, x: 200 }}
-                transition={{ duration: 4 }}
+                initial={{ opacity: 0, y: -100 }} // Initial state
+                animate={{ opacity: 1, y: 0 }} // Animation state
+                exit={{ opacity: 0, y: -100 }} // Exit state
+                transition={{ duration: 1 }} // Animation duration
             >
                 <div className={`project-card-container ${className}`}>
                     <Image width="320px" height="" src={projectImage} alt="project image" />
                     <Heading fontSize="1.3rem">{title}</Heading>
                     <Text color="#A8A8A8">Client:{client}</Text>
-                    <Text color="#A8A8A8">Work: {work}</Text>
+                    <Text color="#A8A8A8">Project Type: {projectType}</Text>
+                    <a className='link' href='#'>Visit site</a>
                 </div>
             </motion.div>
         </>
--- src/Components/Common/SolidButton.jsx
+++ src/Components/Common/SolidButton.jsx
@@ -1,10 +1,10 @@
 import { Button } from "@chakra-ui/react";
 
 
-function SolidButton({ title, onClick, color }) {
+function SolidButton({ title, onClick, color, width }) {
     return (
         <div className="button-container">
-            <Button color={color} fontSize="17px" onClick={onClick} borderRadius={15} w="200px" p="15px 30px" bgColor={"#DD226D"}>{title}</Button>
+            <Button alignItems='center' color={color} fontSize="17px" onClick={onClick} borderRadius={15} w={`200px${width}`} p="15px 30px" bgColor={"#DD226D"}>{title}</Button>
         </div>
     )
 }
