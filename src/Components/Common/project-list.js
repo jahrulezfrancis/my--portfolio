b5ac6998e43cc85bc0b858a779b81ef5341a317b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-09-11 22:17:13 +0100

    Initial commit

---
+++ src/Components/Common/project-list.js
@@ -0,0 +1,32 @@
+export const ProjectList = [
+    {
+        id: 1,
+        clientName: 'Client',
+        projectType: "Web development",
+        siteLink: "#",
+    },
+    {
+        id: 2,
+        clientName: 'Client',
+        projectType: "Web development",
+        siteLink: "#",
+    },
+    {
+        id: 3,
+        clientName: 'Client',
+        projectType: "Web development",
+        siteLink: "#",
+    },
+    {
+        id: 4,
+        clientName: 'Client',
+        projectType: "Web development",
+        siteLink: "#",
+    },
+    {
+        id: 5,
+        clientName: 'Client',
+        projectType: "Web development",
+        siteLink: "#",
+    }
+]
