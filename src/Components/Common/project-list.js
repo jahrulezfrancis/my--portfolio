a6ece1baf62a268725f42b51842c03fc422bab81
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-09-12 16:43:58 +0100

    Adding more project content

--- src/Components/Common/project-list.js
+++ src/Components/Common/project-list.js
@@ -1,32 +1,45 @@
+import proImage from "../../Assets/Images/Img(3).svg"
 export const ProjectList = [
     {
         id: 1,
-        clientName: 'Client',
+        clientName: 'Client 1',
         projectType: "Web development",
+        projectImage: proImage,
         siteLink: "#",
     },
     {
         id: 2,
-        clientName: 'Client',
-        projectType: "Web development",
+        clientName: 'Client 2',
+        projectType: "App development",
+        projectImage: proImage,
         siteLink: "#",
     },
     {
         id: 3,
-        clientName: 'Client',
+        clientName: 'Client 3',
         projectType: "Web development",
+        projectImage: proImage,
         siteLink: "#",
     },
     {
         id: 4,
-        clientName: 'Client',
-        projectType: "Web development",
+        clientName: 'Client 4',
+        projectType: "App development",
+        projectImage: proImage,
         siteLink: "#",
     },
     {
         id: 5,
-        clientName: 'Client',
+        clientName: 'Client 5',
         projectType: "Web development",
+        projectImage: proImage,
         siteLink: "#",
-    }
+    },
+    {
+        id: 4,
+        clientName: 'Client 4',
+        projectType: "App development",
+        projectImage: proImage,
+        siteLink: "#",
+    },
 ]
