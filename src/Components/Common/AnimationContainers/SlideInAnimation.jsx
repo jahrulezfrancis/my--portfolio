07f9103ca2dcb3cb30642315a11f60b089604e78
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-09-18 17:03:47 +0100

    Changed animation type

--- src/Components/Common/AnimationContainers/SlideInAnimation.jsx
+++ src/Components/Common/AnimationContainers/SlideInAnimation.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 export default function SlideInAnimate({ child }) {
     return (
         <motion.div initial={{ scaleX: 0, }}
-            transition={{ duration: 2, type: 'spring' }}
+            transition={{ duration: 1.2, type: 'just' }}
             whileInView={{ scaleX: 1, }}
         >{child}</motion.div>
     )
